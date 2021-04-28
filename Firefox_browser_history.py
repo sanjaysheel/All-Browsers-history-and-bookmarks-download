168f74634127932b6d154bbe10134a26b7a4f6e5
Sanjaysheel <33994199+sanjaysheel@users.noreply.github.com>
2021-04-29 01:43:28 +0530

    Add files via upload

---
+++ Firefox_browser_history.py
@@ -0,0 +1,21 @@
+# ==================================================================
+# you can easily install it using this coommand 
+# pip install browser-history
+# ==================================================================
+
+# ==================================================================
+#                       importing library
+# ==================================================================
+import os
+import time
+from browser_history import get_history
+from browser_history.browsers import Firefox
+from browser_history.browsers import Brave
+from browser_history.browsers import Chrome
+from browser_history.browsers import Chromium
+from browser_history.browsers import Edge
+from browser_history.browsers import Opera
+from browser_history.browsers import OperaGX
+from browser_history.browsers import Safari
+from browser_history.browsers import Vivaldi
+
